10243b0be0edbada8338d56614bf073ec0ba156b
petera_j <petera_j@UCEBNA-NB-11.gjkt.local>
2018-11-19 15:41:44 +0100

    cHaruuu

--- Zklamání v Lese 3.6.1 alpha/src/Main.java
+++ Zklamání v Lese 3.6.1 alpha/src/Main.java
@@ -19,6 +19,8 @@ public class Main extends Application{
 		System.out.print("yay");
 		System.out.print("yay");
 		System.out.print("yay");
+		System.out.print("yay");
+		System.out.print("yay");
 	}
 	
 
