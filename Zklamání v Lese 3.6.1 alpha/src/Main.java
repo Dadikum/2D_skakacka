42451e4fc33d8d0f2df81f46114e0d6cfa00899c
moravec <moravec@UCEBNA-NB-10.gjkt.local>
2018-11-26 16:03:35 +0100

    Vytváření Stage/Scene v Main

--- Zklamání v Lese 3.6.1 alpha/src/Main.java
+++ Zklamání v Lese 3.6.1 alpha/src/Main.java
@@ -1,10 +1,29 @@
-public class Main{
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	private Pane root;
+	
+	private Parent createContent() {
+		root = new Pane();
+		root.setPrefSize(600, 600);
+		
+		return root;
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		stage.setScene(new Scene(createContent()));
+		stage.show();
+	}
 	
 	public static void main(String args[]) {
-		System.out.println("Ahoj");
-		System.out.println("Jak se máš?");
-		System.out.println("Gùùd job chlapci");
-	
+		launch(args);
 	}
 }
 
