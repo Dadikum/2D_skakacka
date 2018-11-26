b219a4a4f27d8e0b7b6af1559c8a791b9c53791c
petera_j <petera_j@UCEBNA-NB-11.gjkt.local>
2018-11-26 15:36:10 +0100

    Love ya bois

--- Zklamání v Lese 3.6.1 alpha/src/Main.java
+++ Zklamání v Lese 3.6.1 alpha/src/Main.java
@@ -3,6 +3,8 @@ public class Main{
 	public static void main(String args[]) {
 		System.out.println("Ahoj");
 		System.out.println("Jak se máš?");
+		System.out.println("Gùùd job chlapci");
+	
 	}
 }
 
